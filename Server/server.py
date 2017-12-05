6ed8b0e1f0daea380ca6eaa6b92bc80781be2c56
Anton Velikodnyy <30283914+Anton-Velikodnyy@users.noreply.github.com>
2017-12-05 14:54:19 -0800

    server works with multiple images
    server works with multiple images now

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import socket
+import base64
 
 
 
@@ -13,23 +14,21 @@ def DisplayImage (img):
     print type(img_np)
     try :
         cv2.imshow("server",img_np)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     except:
         print("Error: Improper image was received")
     print("Press any key to continue...")
-    cv2.waitKey()
 
-def DisplayCredentials (username, password):
+def DisplayCredentials (username):
     print "The following username was received - ",username
-    print "The following password was received - ",password
-    print("Press any key to continue...")
-    cv2.waitKey()
 
 
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip_address = raw_input("Enter ip_address or press enter to use ip address of this computer\n")
 print ("Setting up server...")
-server_socket.bind((ip_address, 5016))
+server_socket.bind((ip_address, 5022))
 server_socket.listen(5)
 print ("Server set up!")
 
@@ -37,34 +36,52 @@ print ("Server set up!")
 client_socket, address = server_socket.accept()
 print "Connected to - ",address,"\n"
 while (1):
-    choice = client_socket.recv(1024)
-    if not choice:
+    print "Waiting for data"
+    choice_str = client_socket.recv(1024)
+    print repr (choice_str)
+    if not choice_str:
         continue
-    choice = int(choice)
+    choice_str = choice_str.split('\n')[0]
+    choice =int(choice_str)
     if(choice == 1):
-        print("Enter your Facebook username")
+        client_socket.send("waiting for username\n");
         username = client_socket.recv(1024)
-        print("Enter your Facebook password")
-        password = client_socket.recv(1024)
-        DisplayCredentials(username=username,password=password)
-        print "Data received successfully"
+        DisplayCredentials(username=username)
+        print "Credentials received successfully"
+        client_socket.send("Credentials received successfully\n");
 
     elif (choice == 2):
+        client_socket.send("waiting for image size\n");
         print "Receiving data"
+        temp_size = client_socket.recv(1024)
+        print repr(temp_size)
+        if not temp_size:
+            continue
+        temp_size = temp_size.split('\n')[0]
+        client_size =int(temp_size)
+        print client_size
         img = ""
+        client_socket.send("waiting for image\n");
 
-        while True:
-            print "recv"
-            str = client_socket.recv(512)
-            print "recved"
-            if not str:
-                break
-            print str
+        server_size = 0
+        while server_size < client_size:
+            str = client_socket.recv(1024)
+            # print ()
+            # print ()
+            # print str
+            # print ()
+            # print ()
             img+= str
+            server_size += len(str)
+            print server_size
 
-        print "Data received successfully"
+        print "Image received successfully"
+        client_socket.send("Image received successfully\n");
+        print len(img)
+        img = base64.b64decode(img)
         DisplayImage(img=img)
-    exit()
+    elif choice == 3:
+        exit()
 
 
 
